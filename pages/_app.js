de60272c4aaae5451ed78eded25478cd39d83237
Ryan Martin <ryan@meraki-digital.com>
2023-07-12 11:48:45 -0500

    auth tweak

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,8 @@ import "../styles/global.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App({ Component, pageProps }) {
-	// If you've used `withAuth`, pageProps.user can pre-populate the hook
-	// if you haven't used `withAuth`, pageProps.user is undefined so the hook
-	// fetches the user from the API routes
-	const { user } = pageProps;
 	return (
-		<UserProvider user={user}>
+		<UserProvider>
 			<Component {...pageProps} />
 		</UserProvider>
 	);
